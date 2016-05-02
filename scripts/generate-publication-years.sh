6fd38907d7ac4f053fe3cd745c919c2e68dbddf3
Lincoln Mullen <lincoln@lincolnmullen.com>
2016-05-02 10:07:55 -0400

    Generate publication/year combos

---
+++ scripts/generate-publication-years.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Create a list of absolute paths for the combinations of publication and year
+# in the Chronicling America dataset.
+
+OCRDIR="/Volumes/RESEARCH/chronicling-america/ocr"
+find $OCRDIR -mindepth 2 -maxdepth 2 -type d 
