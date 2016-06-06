3be8dcc4fb188526608712248fa4bdfb6c93b20c
Lincoln Mullen <lincoln@lincolnmullen.com>
2016-06-06 14:44:59 -0400

    Add data

---
+++ _scripts/data-export.R
@@ -0,0 +1,12 @@
+suppressPackageStartupMessages(library(dplyr))
+suppressPackageStartupMessages(library(readr))
+
+message("Reading data")
+
+quotations <- read_csv("quotations-clean.csv", col_types = "ccnnnncnciicccDiiccc")
+
+message("Writing export")
+
+quotations %>%
+  select(-token_count, -tfidf, -proportion, -runs_pval) %>%
+  write_csv("public-bible-quotations.csv")
