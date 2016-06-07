93d1bd0c2aec9216bd19748a9e66b84fcfb700d5
Lincoln Mullen <lincoln@lincolnmullen.com>
2016-06-07 17:06:46 -0400

    iframe resizing

--- _verse-explorer/ui.R
+++ _verse-explorer/ui.R
@@ -3,8 +3,9 @@ library(shinythemes)
 library(dygraphs)
 
 shinyUI(fluidPage(
-  theme = "wwww/bootstrap.min.css",
+  theme = "bootstrap.min.css",
   tags$head(includeCSS("www/style.css")),
+  tags$head(includeScript("www/iframeResizer.contentWindow.min.js")),
   fluidRow(
     column(9, dygraphOutput("verse_ts_chart")),
     column(3, div(id = "verse-ts-labels"))
