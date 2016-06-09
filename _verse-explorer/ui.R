58e1913cdfbe0d239faf766753521ba9ba33b26e
Lincoln Mullen <lincoln@lincolnmullen.com>
2016-06-09 17:03:57 -0400

    Tweaks to selectize

--- _verse-explorer/ui.R
+++ _verse-explorer/ui.R
@@ -7,7 +7,6 @@ shinyUI(fluidPage(
   theme = "bootstrap.min.css",
   tags$head(includeCSS("www/style.css")),
   tags$head(includeScript("www/iframeResizer.contentWindow.min.js")),
-  tags$head(includeScript("www/query-string.js")),
   fluidRow(
     column(9, dygraphOutput("verse_ts_chart")),
     column(3, div(id = "verse-ts-labels"))
@@ -26,15 +25,21 @@ shinyUI(fluidPage(
            actionLink("collection_proverbs", "Proverbs (most quoted)"),
            actionLink("collection_psalms", "Psalms (most quoted)"),
            actionLink("collection_revelation", "Book of Revelation (most quoted)"),
+           actionLink("collection_prophets", "Prophets (most quoted)"),
            actionLink("collection_words_of_jesus", "Words of Jesus (most quoted)"),
            actionLink("collection_goldrenrule", "Golden rule"),
            actionLink("collection_deafness", "Deafness"),
            actionLink("collection_death", "Death and resurrection"),
+           actionLink("collection_temperance", "Temperance"),
            actionLink("collection_genesis1", "Genesis 1")
            )),
   fluidRow(selectizeInput("references", "References", references,
                           selected = c("Acts 17:26", "John 3:16"),
-                          multiple = TRUE)),
+                          multiple = TRUE,
+                          options = list(
+                            maxItems = 10,
+                            maxOptions = 3
+                          ))),
   fluidRow(htmlOutput("verse_text")),
   fluidRow(id = "quotations-table",
            tags$h3("Pages with selected verses"),
