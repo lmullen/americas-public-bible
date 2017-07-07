589aecd816ad540bbfd9a8f7ae54d4944bf0b00f
Lincoln Mullen <lincoln@lincolnmullen.com>
2017-07-07 14:52:46 -0400

    Move verse explorer to its own repository

--- _verse-explorer/global.R
+++
@@ -1,6 +0,0 @@
-verses_by_year <- readRDS("../_data/verses-by-year.rds")
-bible_by_year <- readRDS("../_data/bible-by-year.rds")
-bible_verses <- readRDS("../_data/bible-verses.rds")
-quotations_df <- readRDS("../_data/quotations-for-shiny.rds")
-
-references <- sort(unique(verses_by_year$reference))
