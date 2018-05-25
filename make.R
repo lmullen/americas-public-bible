67e046fa6e2f14b5e3d2523c11d193add3502e80
Lincoln Mullen <lincoln@lincolnmullen.com>
2018-05-25 10:27:13 -0400

    KJV

--- make.R
+++ make.R
@@ -1,15 +1,21 @@
 # The drake plan for processing all the data files
 
-library(drake)
 suppressPackageStartupMessages(library(tidyverse))
+library(drake)
+library(fs)
 
 pkgconfig::set_config("drake::strings_in_dots" = "literals")
 
 source("R/lds.R")
+source("R/kjv.R")
 
 plan <- drake_plan(
-  lds_raw = read_csv(file_in("raw/lds-scriptures.csv"), col_types = "iiiicccccccccciiccc"),
-  process_lds(lds_raw, file_out("data/lds.csv"))
+  lds_raw = read_csv(file_in("raw/lds/lds-scriptures.csv"), col_types = "iiiicccccccccciiccc"),
+  process_lds(lds_raw, file_out("data/lds.csv")),
+  parse_kjv(file_out("data/kjv.csv"))
 )
+
+config <- drake_config(plan)
+vis_drake_graph(config)
 
 make(plan)
