da5cfb395458373fcc96775e9553fb1c8837eba3
Lincoln Mullen <lincoln@lincolnmullen.com>
2018-05-25 10:26:16 -0400

    Revise format of LDS scriptures

--- R/lds.R
+++ R/lds.R
@@ -3,8 +3,8 @@
 download_lds <- function() {
   dir.create("raw", showWarnings = FALSE)
   download.file("http://scriptures.nephi.org/downloads/lds-scriptures.csv.zip",
-                "raw/lds-scriptures.csv.zip")
-  unzip("raw/lds-scriptures.csv.zip", exdir = "raw")
+                "raw/lds/lds-scriptures.csv.zip")
+  unzip("raw/lds/lds-scriptures.csv.zip", exdir = "raw")
 }
 
 process_lds <- function(data, out_csv) {
@@ -12,13 +12,12 @@ process_lds <- function(data, out_csv) {
     filter(volume_title != "Old Testament",
            volume_title != "New Testament") %>%
     mutate(part = volume_title) %>%
-    select(version = volume_title,
+    select(doc_id = verse_title,
+           version = volume_title,
            part = part,
            book = book_title,
            chapter = chapter_number,
            verse = verse_number,
-           doc_id = verse_title,
-           citation = verse_short_title,
            text = scripture_text)
 
   dir.create(dirname(out_csv), showWarnings = FALSE)
