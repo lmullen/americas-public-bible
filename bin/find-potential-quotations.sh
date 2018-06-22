b31c9d7b7e451b1323131bae9e99b1fbc49083d3
Lincoln Mullen <lincoln@lincolnmullen.com>
2018-06-22 10:14:03 -0400

    Update memory and job limits

--- bin/find-potential-quotations.sh
+++ bin/find-potential-quotations.sh
@@ -4,9 +4,9 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=lmullen@gmu.edu
 #SBATCH --partition=all-HiPri
-#SBATCH --mem-per-cpu=12G
+#SBATCH --mem-per-cpu=24G
 #SBATCH --export=NONE
-##SBATCH --array=1-5989%60
+#SBATCH --array=1-5989
 
 ## Load modules since we are not exporting our environment
 module load R/3.4.4
