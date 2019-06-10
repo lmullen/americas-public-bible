e4985e338d390d482b0b29ab15b58972fb7c38a0
Lincoln Mullen <lincoln@lincolnmullen.com>
2019-06-10 16:17:01 -0400

    Limit array

--- bin/find-potential-quotations.sh
+++ bin/find-potential-quotations.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #SBATCH --job-name=find-potential-quotations
-#SBATCH --output="/scratch/lmullen/logs/%A_%a.out"
+#SBATCH --output="/scratch/lmullen/logs/%A_%a.log"
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=lmullen@gmu.edu
 #SBATCH --partition=all-HiPri
 #SBATCH --ntasks=1
-#SBATCH --mem=12G
+#SBATCH --mem=24G
 #SBATCH --export=NONE
-#SBATCH --array=1-14986
+#SBATCH --array=1-14986%5
 
 ## Load modules since we are not exporting our environment
 module load R/3.5.2
