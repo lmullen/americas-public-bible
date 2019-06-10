7c083f93aa2a07a0aa5ff0220ad5ae2c0d443846
Lincoln Mullen <lincoln@lincolnmullen.com>
2019-06-10 12:00:41 -0400

    Update array task script

--- bin/find-potential-quotations.sh
+++ bin/find-potential-quotations.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 #SBATCH --job-name=find-potential-quotations
-#SBATCH --output="/scratch/lmullen/logs/find-quotations-%A_%a.out"
+#SBATCH --output="/scratch/lmullen/logs/%A_%a.out"
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=lmullen@gmu.edu
 #SBATCH --partition=all-HiPri
-#SBATCH --mem-per-cpu=24G
+#SBATCH --ntasks=1
+#SBATCH --mem=12G
 #SBATCH --export=NONE
-#SBATCH --array=1-1499
+#SBATCH --array=1-14986
 
 ## Load modules since we are not exporting our environment
 module load R/3.5.2
