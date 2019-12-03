8ca5910ea1b0f962f6159346e93876728c471605
Lincoln Mullen <lincoln@lincolnmullen.com>
2019-12-03 17:25:20 -0500

    Remove old deploy script

--- deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-set -x
-Rscript -e "packrat::snapshot()"
-Rscript -e "packrat::bundle(file = 'packrat/bundles/verse-explorer-deploy.tar.gz', overwrite = TRUE)"
-scp packrat/bundles/verse-explorer-deploy.tar.gz wat:/srv/shiny-server/
