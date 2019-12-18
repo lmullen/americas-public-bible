42ebc364b321b85948afdd7d2da945e9811f772b
Lincoln Mullen <lincoln@lincolnmullen.com>
2019-12-18 16:06:42 -0500

    Delete scripts cruft

--- _scripts/data-export.R
+++
@@ -1,8 +0,0 @@
-suppressPackageStartupMessages(library(dplyr))
-suppressPackageStartupMessages(library(readr))
-
-quotations <- readRDS("_data/quotations-clean.rds")
-
-quotations %>%
-  select(-token_count, -tfidf, -proportion, -runs_pval) %>%
-  write_csv("public-bible-quotations.csv")
