08129d7c10ad5b69b00aa05f09a4efc3cd7fba2c
Lincoln Mullen <lincoln@lincolnmullen.com>
2020-07-01 17:20:13 -0400

    Fix Webpack polling problem

--- website/webpack.config.js
+++ website/webpack.config.js
@@ -3,23 +3,22 @@ const path = require('path');
 module.exports = {
   mode: 'production',
   entry: {
-    "verse-viewer": "./viz/verse-viewer/main.js",
+    'verse-viewer': path.resolve(__dirname, 'viz/verse-viewer/main.js'),
   },
   output: {
     path: path.resolve(__dirname, 'assets/viz/'),
+  },
+  watchOptions: {
+    aggregateTimeout: 300,
+    poll: 1000,
+    ignored: path.resolve(__dirname, 'node_modules'),
   },
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader',
-        ],
+        use: ['style-loader', 'css-loader'],
       },
     ],
   },
-  resolve: {
-    modules: ['node_modules']
-  }
 };
