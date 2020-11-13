830dcc514d5c81aaf747dc135ea8684924f4d74d
Lincoln Mullen <lincoln@lincolnmullen.com>
2020-11-12 19:40:05 -0500

    Use production API

--- website/assets/viz/config.js
+++ website/assets/viz/config.js
@@ -1,5 +1,5 @@
 const config = {
-  API_BASE: 'http://localhost:8090',
+  API_BASE: 'https://data.chnm.org',
   CHRONAM_PAGE_BASE: 'https://chroniclingamerica.loc.gov/lccn',
   MILLIONS: 100, // rate for y-axis of trend lines
 };
