5bb106d9f5ceede2a5212963b0be4fdc12b9d0f0
Lincoln Mullen <lincoln@lincolnmullen.com>
2020-11-13 11:18:47 -0500

    Slightly darker colors for the sparklines

--- website/assets/viz/common/color-scale.js
+++ website/assets/viz/common/color-scale.js
@@ -1,7 +1,7 @@
 import * as d3 from 'd3';
 
 const colorScale = d3
-  .scaleSequential((t) => d3.hsl(t * 360, 1, 0.5).toString())
+  .scaleSequential((t) => d3.hsl(t * 360, 1, 0.425).toString())
   .domain([1, 36]);
 
 export default colorScale;
