aea3dc1cfb16a53cffd168a558b93e24a0be3eec
Lincoln Mullen <lincoln@lincolnmullen.com>
2021-02-05 11:48:41 -0500

    Add biblical order viewer

---
+++ website/assets/viz/verse-index/biblical-order.js
@@ -0,0 +1,20 @@
+import * as d3 from 'd3';
+import IndexItem from '../common/index-item';
+import config from '../config';
+
+const topPromise = d3.json(`${config.API_BASE}/apb/index/biblical/`);
+
+topPromise
+  .then((data) => {
+    console.log(data);
+    data.forEach((d) => {
+      const item = new IndexItem(
+        '#biblical-order',
+        d.reference,
+        d.text,
+        d.count
+      );
+      item.render();
+    });
+  })
+  .catch((e) => console.log(e));
