cb87c96142c90baefb2364cfea697a4f587eaaf2
Lincoln Mullen <lincoln@lincolnmullen.com>
2021-09-01 11:28:42 -0400

    Use API not localhost for verse select

--- website/assets/viz/config.js
+++ website/assets/viz/config.js
@@ -1,6 +1,6 @@
 const config = {
-  API_BASE: 'http://localhost:8090',
-  // API_BASE: 'https://data.chnm.org',
+  // API_BASE: 'http://localhost:8090',
+  API_BASE: 'https://data.chnm.org',
   CHRONAM_PAGE_BASE: 'https://chroniclingamerica.loc.gov/lccn',
   MILLIONS: 100, // rate for y-axis of trend lines
 };
