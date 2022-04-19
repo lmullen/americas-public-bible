15d3c8ba27a2358133b9333ba9562d608d34f6c2
Lincoln Mullen <lincoln@lincolnmullen.com>
2022-04-19 15:04:30 -0400

    Allow users to search text of verses

--- website/assets/viz/config.js
+++ website/assets/viz/config.js
@@ -1,7 +1,7 @@
 const config = {
-  // API_BASE: 'http://localhost:8090',
-  API_BASE: 'https://data.chnm.org',
-  CHRONAM_PAGE_BASE: 'https://chroniclingamerica.loc.gov/lccn',
+  // API_BASE: "http://localhost:8090",
+  API_BASE: "https://data.chnm.org",
+  CHRONAM_PAGE_BASE: "https://chroniclingamerica.loc.gov/lccn",
   MILLIONS: 100, // rate for y-axis of trend lines
 };
 export default config;
