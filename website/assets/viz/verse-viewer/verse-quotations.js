94e9f8979e1da3ffd4087ac6d0e38aa450070116
Lincoln Mullen <lincoln@lincolnmullen.com>
2022-04-26 14:30:08 -0400

    Fix table width

--- website/assets/viz/verse-viewer/verse-quotations.js
+++ website/assets/viz/verse-viewer/verse-quotations.js
@@ -1,13 +1,13 @@
-import * as d3 from 'd3';
-import config from '../config';
-import { year, day, probLabel } from '../common/display';
-import { wordsForUrl } from '../common/text';
+import * as d3 from "d3";
+import config from "../config";
+import { year, day, probLabel } from "../common/display";
+import { wordsForUrl } from "../common/text";
 
 export default class VerseQuotations {
   constructor(id, reference, verseText) {
     this.node = d3.select(id);
     this.data = null;
-    this.status = 'loading';
+    this.status = "loading";
     const ref = encodeURIComponent(reference);
     this.url = `${config.API_BASE}/apb/verse-quotations?ref=${ref}`;
     this.keywords = wordsForUrl(verseText);
@@ -16,14 +16,14 @@ export default class VerseQuotations {
   async fetch() {
     try {
       this.data = await d3.json(this.url);
-      this.status = 'ok';
+      this.status = "ok";
     } catch (e) {
-      if (e.message === '404 Not Found') {
+      if (e.message === "404 Not Found") {
         console.log(e);
-        this.status = 'missing';
+        this.status = "missing";
       } else {
         console.log(e);
-        this.status = 'failed';
+        this.status = "failed";
       }
     }
   }
@@ -31,30 +31,32 @@ export default class VerseQuotations {
   async render() {
     await this.fetch();
 
-    if (this.status !== 'ok') {
+    if (this.status !== "ok") {
       this.node.remove();
       return this.status;
     }
 
-    const t = this.node.append('table').classed('hover', true);
+    const t = this.node.append("table").classed("hover", true);
 
-    t.append('thead').append('tr').html(
-      `<th>Year</th>
-      <th style="min-width:10%">Date</th>
+    t.append("thead")
+      .append("tr")
+      .html(
+        `<th>Year</th>
+      <th style="min-width:80px;">Date</th>
       <th>Newspaper</th>
       <th style="min-width:20%">State</th>
       <th>Certainty</th>
       <th>Context</th>`
-    );
+      );
 
     // Here this.table is actually the <tbody> node
-    this.table = t.append('tbody');
+    this.table = t.append("tbody");
 
     this.table
-      .selectAll('tr')
+      .selectAll("tr")
       .data(this.data, (d) => d.doc_id)
       .enter()
-      .append('tr')
+      .append("tr")
       .html(
         (d) =>
           `<td>${year(d.date)}</td>
@@ -62,9 +64,7 @@ export default class VerseQuotations {
           <td>${d.title}</td>
           <td>${d.state}</td>
           <td>${probLabel(d.probability)}</td>
-          <td><a href="${this.chronamQuery(
-            d.docID
-          )}">ChronAm&nbsp;&rarr;</a></td>`
+          <td><a href="${this.chronamQuery(d.docID)}">ChronAm&nbsp;&rarr;</a></td>`
       );
 
     return this.status;
